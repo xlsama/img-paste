ab6c682a593c1ed2c2a1660585cf751994c2e7a4
xlsama <im.xlsama@gmail.com>
2023-06-02 18:26:06 +0800

    chore: update sha256

--- Formula/img-paste.rb
+++ Formula/img-paste.rb
@@ -2,7 +2,7 @@ class ImgPaste < Formula
   desc "Command-line tool to save image from clipboard to file"
   homepage "https://github.com/xlsama/img-paste"
   url "https://github.com/xlsama/img-paste/releases/download/v1.0.0/img-paste-1.0.0.tar.gz"
-  sha256 "a3be60e71306a207c9453a4ab6f049c3d36f38640304dc3dbc5814f299bd5fb1"
+  sha256 "b31b55368d491a830b4b3bd8ce87d0f599682e7505c386b2567d71da6dda9ee0"
   license "MIT"
 
   def install
